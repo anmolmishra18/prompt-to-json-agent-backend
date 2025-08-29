e206e6752f1ef6373cefcd1d1239d2bcc3971fdb
Anmol Mishra <anmolmishra307680@gmail.com>
2025-08-29 11:09:40 +0530

    🧹 Clean up: remove unused files, add .gitignore

--- start.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-"""Simple startup script for the FastAPI server"""
-
-import uvicorn
-
-if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
